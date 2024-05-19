839eeb34989988ae74efbabdf1cb1fa94f782832
dir-FY24takahashikeisuke <keisuke.takahashi.c980@dirbato.co.jp>
2024-05-19 09:46:13 +0900

    いろいろやりました。

---
+++ Ksuke/chap4/Main.java
@@ -0,0 +1,11 @@
+public class Main{
+    public static void mian (String[] args){
+        int array[][] = new int[][]{(1, 2), (2, 3, 4)};
+        int total = 0;
+        for (int i = 0; i <array.length; i++){
+            for(int j = i; j < array[i].length; j++){
+                total += array[i].length; j++
+            }
+        }
+    }
+}
